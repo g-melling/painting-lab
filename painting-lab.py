99475be38a96962b4a58ecbe43204ec7a3319d65
Georgina Melling <156523659+georginamelling@users.noreply.github.com>
2026-08-23 22:18:03 +0100

    Image loading, mirror and greyscale added

--- painting-lab.py
+++ painting-lab.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from PIL import Image, ImageOps
+
+def load_image(path):
+    return Image.open(path)
+
+def mirror_image(image):
+    return ImageOps.mirror(image)
+
+def grayscale_image(image):
+    return ImageOps.grayscale(image)
